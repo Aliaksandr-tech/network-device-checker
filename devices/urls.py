00435cadf374dfc7c6671959208020c83a3b4d46
Admin Test <admintest@Admins-MacBook-Air.local>
2025-07-01 22:23:51 +0300

    Отключены лишние маршруты. Реализована кнопка возврата на главную со страницы устройства

--- devices/urls.py
+++ devices/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import cli_auth_view
 
 urlpatterns = [
-    path('', views.index, name='index'), # шаблон стартовой
-    path('search/', views.device_search, name='device_search'),
+    # path('', views.index, name='index'), # шаблон стартовой, # заглушка
+    # path('search/', views.device_search, name='device_search'), # заглушка
     path('device/<int:device_id>/', views.device_detail, name='device_detail'),
     path('feature/<int:feature_id>/method/', views.feature_method_view, name='feature_method'),
     path('device/<int:device_id>/ping/', views.ping_device, name='ping_device'),
