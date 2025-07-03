fa30e39a9b52966cd73bdcf295e55fddbacd29ae
Admin Test <admintest@Admins-MacBook-Air.local>
2025-07-03 17:34:02 +0300

    Реализовал маршруты для MEDIA

--- ndc/urls.py
+++ ndc/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('devices/', include('devices.urls')),
     path('', include('devices.urls')),
     # другие приложения
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
